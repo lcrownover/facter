664aaf5e5374635a59237f5fec7b3173217ab9b0
Lucas Crownover <lcrown@uoregon.edu>
2019-11-01 08:59:54 -0700

    testing set_fact

--- tasks/set_fact.rb
+++ tasks/set_fact.rb
@@ -4,28 +4,28 @@ require 'open3'
 require 'puppet'
 
 def set_fact(name, value)
-  # Cross platform to set facts_dir
-  # if Facter.value(:os)['family'] == 'windows'
-  #   facts_dir = 'C:\\ProgramData\\PuppetLabs\\facter\\facts.d\\'
-  # else
-  #   facts_dir = '/etc/puppetlabs/facter/facts.d/'
-  # end
+  Cross platform to set facts_dir
+  if Facter.value(:os)['family'] == 'windows'
+    facts_dir = 'C:\\ProgramData\\PuppetLabs\\facter\\facts.d\\'
+  else
+    facts_dir = '/etc/puppetlabs/facter/facts.d/'
+  end
 
-  # # Set some vars in a readable location
-  # file_name     = "#{name}.yaml"
-  # file_content  = "#{name}: '#{value}'\n"
-  # file_path     = "#{facts_dir}#{file_name}"
+  # Set some vars in a readable location
+  file_name     = "#{name}.yaml"
+  file_content  = "#{name}: '#{value}'\n"
+  file_path     = "#{facts_dir}#{file_name}"
 
-  # # Create a file in facts_dir with the correct name and data
-  # File.open(file_path, 'w'){|f| f.write(file_content)}
-  # raise Puppet::Error, _("stderr": "Failed to write to path: #{file_path}") unless File.exist?(file_path)
+  # Create a file in facts_dir with the correct name and data
+  File.open(file_path, 'w'){|f| f.write(file_content)}
+  raise Puppet::Error, _("stderr": "Failed to write to path: #{file_path}") unless File.exist?(file_path)
 
-  # # Test the fact to see if it's working
-  # stdout, stderr, status = Open3.capture3(['facter',name])
-  # raise Puppet::Error, _("stderr: ' %{stderr}') % { stderr: stderr }") if status.exitstatus != 0
+  # Test the fact to see if it's working
+  stdout, stderr, status = Open3.capture3(['facter',name])
+  raise Puppet::Error, _("stderr: ' %{stderr}') % { stderr: stderr }") if status.exitstatus != 0
 
-  # # format a result and return it
-  # { status: 'success' }
+  # format a result and return it
+  { status: 'success' }
 end
 
 # Find the desired setting from the JSON coming in over STDIN
@@ -36,10 +36,11 @@ value  = params['value']
 # Run the command with the desired setting, and return the result
 begin
   result = set_fact(name, value)
+  puts result.class
   puts result.to_json
-  # exit 0
+  exit 0
 rescue Puppet::Error => e
-  puts({ status: 'failure', error: e.message }.to_json)
+  puts({ _error: { kind: 'facter_task/failure', msg: e.message }}.to_json)
   exit 1
 rescue Exception => e
   puts e
